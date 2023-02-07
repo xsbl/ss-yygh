0af64a41dacbcbe0247ad33f23ce85f0a254e78f
sisheng <2843643165@qq.com>
2023-02-07 22:44:36 +0800

    全局统一异常处理和自定义异常

--- common/common_util/src/main/java/com/sisheng/yygh/common/exception/GlobalExceptionHandler.java
+++ common/common_util/src/main/java/com/sisheng/yygh/common/exception/GlobalExceptionHandler.java
@@ -3,23 +3,16 @@ package com.sisheng.yygh.common.exception;
 import com.sisheng.yygh.common.result.Result;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * @author bobochang
- * @description
- * @created 2022/6/29-20:23
- **/
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    @ResponseBody
     public Result error(Exception e){
         e.printStackTrace();
         return Result.fail();
     }
 
     @ExceptionHandler(YyghException.class)
-    @ResponseBody
     public Result error(YyghException e){
         e.printStackTrace();
         return Result.fail();
--- common/common_util/src/main/java/com/sisheng/yygh/common/exception/YyghException.java
+++ common/common_util/src/main/java/com/sisheng/yygh/common/exception/YyghException.java
@@ -17,20 +17,11 @@ public class YyghException extends RuntimeException {
     @ApiModelProperty(value = "异常状态码")
     private Integer code;
 
-    /**
-     * 通过状态码和错误消息创建异常对象
-     * @param message
-     * @param code
-     */
     public YyghException(String message, Integer code) {
         super(message);
         this.code = code;
     }
 
-    /**
-     * 接收枚举类型对象
-     * @param resultCodeEnum
-     */
     public YyghException(ResultCodeEnum resultCodeEnum) {
         super(resultCodeEnum.getMessage());
         this.code = resultCodeEnum.getCode();
