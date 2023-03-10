cd5d7e272f54a8e37f049ebc2de663113af07981
sisheng <2843643165@qq.com>
2023-03-10 21:50:03 +0800

    把service-hosp和service-cmn注册到nacos

--- service/service_cmn/src/main/java/com/sisheng/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/sisheng/yygh/cmn/ServiceCmnApplication.java
@@ -4,20 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * @author bobochang
- * @description
- * @created 2022/7/1-14:51
- **/
-//@SpringBootApplication(exclude = MongoAutoConfiguration.class)
-//@ComponentScan(basePackages = "com.sisheng")
-//@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.sisheng.yygh.cmn.mapper")
 @ComponentScan("com.sisheng.yygh")
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class, args);
