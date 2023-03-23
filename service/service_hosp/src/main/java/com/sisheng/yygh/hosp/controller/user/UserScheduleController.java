f80567c48c80fba0120f8817bbcbdcfbca45fd01
sisheng <2843643165@qq.com>
2023-03-23 22:00:56 +0800

    查询排班详情信息

--- service/service_hosp/src/main/java/com/sisheng/yygh/hosp/controller/user/UserScheduleController.java
+++ service/service_hosp/src/main/java/com/sisheng/yygh/hosp/controller/user/UserScheduleController.java
@@ -3,12 +3,14 @@ package com.sisheng.yygh.hosp.controller.user;
 import com.sisheng.yygh.common.result.Result;
 import com.sisheng.yygh.hosp.service.ScheduleService;
 import com.sisheng.yygh.model.hosp.Hospital;
+import com.sisheng.yygh.model.hosp.Schedule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,6 +28,15 @@ public class UserScheduleController {
         Map<String, Object> hospital = scheduleService.getBookingScheduleRule(page, limit, hoscode, depcode );
 
         return Result.ok(hospital);
+    }
+
+    @GetMapping("/{hoscode}/{depcode}/{workdate}")
+    public Result getScheduleDetail(@PathVariable String hoscode,
+                                                       @PathVariable String depcode,
+                                                       @PathVariable String workdate) {
+        List< Schedule > schedules = scheduleService.getDetailSchedule(hoscode, depcode, workdate );
+
+        return Result.ok(schedules);
     }
 
 }
