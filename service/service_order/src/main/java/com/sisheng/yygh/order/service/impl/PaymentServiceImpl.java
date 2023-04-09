8e6f1b92dfd2b696b35335ad846825cf8badc1d0
sisheng <2843643165@qq.com>
2023-04-09 15:51:03 +0800

    保存支付记录

--- service/service_order/src/main/java/com/sisheng/yygh/order/service/impl/PaymentServiceImpl.java
+++ service/service_order/src/main/java/com/sisheng/yygh/order/service/impl/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import com.sisheng.yygh.model.order.OrderInfo;
 import com.sisheng.yygh.model.order.PaymentInfo;
 import com.sisheng.yygh.order.mapper.PaymentMapper;
 import com.sisheng.yygh.order.service.PaymentService;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,21 +19,21 @@ public class PaymentServiceImpl extends ServiceImpl<PaymentMapper, PaymentInfo> 
  */
     @Override
     public void savePaymentInfo(OrderInfo orderInfo, Integer paymentType) {
-//         QueryWrapper<PaymentInfo> queryWrapper = new QueryWrapper<>();
-//         queryWrapper.eq("order_id", orderInfo.getId());
-//         queryWrapper.eq("payment_type", paymentType);
-//         Integer count = baseMapper.selectCount(queryWrapper);
-//         if(count >0) return;
-//         // 保存交易记录
-//         PaymentInfo paymentInfo = new PaymentInfo();
+        //有可能生成二维码的时候没有支付，关闭了弹窗；然后又一次打开了弹窗 -- 防止对同一个订单生成多个支付记录
+         QueryWrapper<PaymentInfo> queryWrapper = new QueryWrapper<>();
+         queryWrapper.eq("order_id", orderInfo.getId());
+         Integer count = baseMapper.selectCount(queryWrapper);
+         if(count >0) return;
+         // 保存交易记录
+         PaymentInfo paymentInfo = new PaymentInfo();
 //         paymentInfo.setCreateTime(new Date());
-//         paymentInfo.setOrderId(orderInfo.getId());
-//         paymentInfo.setPaymentType(paymentType);
-//         paymentInfo.setOutTradeNo(orderInfo.getOutTradeNo());
-//         paymentInfo.setPaymentStatus(PaymentStatusEnum.UNPAID.getStatus());
-//         String subject = new DateTime(orderInfo.getReserveDate()).toString("y
-//                24 paymentInfo.setSubject(subject);
-//         paymentInfo.setTotalAmount(orderInfo.getAmount());
-//         baseMapper.insert(paymentInfo);
+         paymentInfo.setOrderId(orderInfo.getId());
+         paymentInfo.setPaymentType(paymentType);
+         paymentInfo.setOutTradeNo(orderInfo.getOutTradeNo());
+         paymentInfo.setPaymentStatus(PaymentStatusEnum.UNPAID.getStatus());
+         String subject = new DateTime(orderInfo.getReserveDate()).toString("yyyy-MM-dd")+"|"+orderInfo.getHosname()+"|"+orderInfo.getDepname()+"|"+orderInfo.getTitle();
+         paymentInfo.setSubject(subject);
+         paymentInfo.setTotalAmount(orderInfo.getAmount());
+         baseMapper.insert(paymentInfo);
          }
  }
